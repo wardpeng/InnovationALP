fa240c2367e161f4eb3e8b4bf9b9ed056fa0a4f5
I327500 <ward.peng@sap.com>
2020-06-12 04:24:08 +0000

    Draft version Backup 20200612_12:34

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/generic/app/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("com.sap.sdc.innovation.knowledgebase.alp.zinnov_knowledgebase_alp.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
