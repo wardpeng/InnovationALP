4387b1fc6b19b1704cf4bc3618eab2ec70cfacb5
I327500 <ward.peng@sap.com>
2020-06-18 06:09:29 +0000

    Backup before refactor

--- webapp/serviceBinding.js
+++ webapp/serviceBinding.js
@@ -1,5 +1,5 @@
 function initModel() {
-	var sUrl = "/sap/opu/odata/sap/ZWPINNOV_C_IDEA_VIEWED_Q_CDS/";
+	var sUrl = "/sap/opu/odata/sap/ZINNOV_C_IDEA_VIEWED_QUERY_CDS/";
 	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
 	sap.ui.getCore().setModel(oModel);
 }
